d9f2d0ef7df26652dec3f9c6ad26d1137dca1cfd
Jupiter <jwestbard@gmail.com>
2021-09-12 15:55:17 -0400

    maze visual overhaul

--- node-express/public/maze/constants.js
+++ node-express/public/maze/constants.js
@@ -5,8 +5,8 @@ const drawArr = {
     s: [{x: 1, y: 1}, {x: -1, y: 1}],
 }
 
-const padding = 100
-const cellWidth = 40
+const padding = {x: 200, y: 100}
+const cellWidth = 35
 
 const mazeSize = 15
 var dirKeys = ['w', 'e', 'n', 's']
@@ -37,4 +37,9 @@ const traceArr = []
 const maxTime = 15
 const trailLength = 20
 
+const wasdImg = document.getElementById('wasd')
+const generateImg = document.getElementById('generate')
+
+var buttonFade = false
+var buttonGradient = 0
 var player
--- node-express/public/maze/maze.js
+++ node-express/public/maze/maze.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
+canvas.width = innerWidth
+canvas.height = innerHeight
 
 Array.prototype.shuffle = function() { // imported this function
     let currentIndex = this.length, randomIndex
@@ -74,6 +76,34 @@ function tryKeys() {
             // BREAK THE WALLS
             grid[ant.y][ant.x].breakWall(key)
             grid[ant.y][ant.x].visit()
+            // visually break the walls
+            c.beginPath()
+            let penBreak = {
+                x: padding.x + (cellWidth * ant.x) + (cellWidth / 2),
+                y: padding.y + (cellWidth * ant.y) + (cellWidth / 2)
+            }
+            c.moveTo(penBreak.x + ((cellWidth / 2) * drawArr[key][0].x),
+                     penBreak.y + ((cellWidth / 2) * drawArr[key][0].y))
+            c.lineTo(penBreak.x + ((cellWidth / 2) * drawArr[key][1].x),
+                     penBreak.y + ((cellWidth / 2) * drawArr[key][1].y))
+            c.stroke()
+            
+            c.beginPath()
+            c.arc(
+                penBreak.x + ((cellWidth / 2) * drawArr[key][0].x),
+                penBreak.y + ((cellWidth / 2) * drawArr[key][0].y),
+                4, 0, (2*Math.PI), false
+            )
+            c.fill()
+
+            c.beginPath()
+            c.arc(
+                penBreak.x + ((cellWidth / 2) * drawArr[key][1].x),
+                penBreak.y + ((cellWidth / 2) * drawArr[key][1].y),
+                4, 0, (2*Math.PI), false
+            )
+            c.fill()
+            //
             ant = {
                 x: ant.x + dirDict[key].x,
                 y: ant.y + dirDict[key].y
@@ -85,6 +115,8 @@ function tryKeys() {
             else if (key == 's') {flipKey = 'n'}
             grid[ant.y][ant.x].breakWall(flipKey)
             grid[ant.y][ant.x].visit()
+            //
+
         }
         else {
             ignored++
@@ -93,74 +125,6 @@ function tryKeys() {
     return (ignored == 4) ? true : false
 }
 
-function generate() { // begin maze generation
-// while (!grid[mazeSize-1][mazeSize-1].visited) {} // to prevent total blocking
-
-    grid = initGrid()
-
-    ant = {x: 0, y: 0}
-    log = ['quit', {x: 0, y: 0}]
-
-    while (log[log.length - 1] != 'quit') {
-        // cell loop here
-        dirKeys.shuffle()
-
-        const trace = tryKeys()
-
-        if (trace) {
-            log.pop()
-            ant = {
-                x: log[log.length - 1].x,
-                y: log[log.length - 1].y
-            }
-        }
-        else {
-            log.push({
-                x: ant.x,
-                y: ant.y
-            })
-        }
-    }
-}
-
-Array.prototype.drawGrid = function() {
-    c.strokeStyle = '#EEEEEE'
-    c.fillStyle = '#EEEEEE'
-    c.lineWidth = 8
-
-    grid.forEach((row, i) => {
-        row.forEach((cell, j) => {
-            const center = {
-                x: 300 + (cellWidth * j), // wouldve used padding, no time
-                y: padding + (cellWidth * i)
-            }
-            dirKeys.forEach(key => {
-                if (cell.walls[key] &&
-                    !((i == 0 && j == 0) && key == 'w') &&
-                    !((i == mazeSize-1 && j == mazeSize-1) && key == 'e')) {
-                    c.beginPath()
-                    c.moveTo(
-                        center.x + ((cellWidth / 2) * drawArr[key][0].x),
-                        center.y + ((cellWidth / 2) * drawArr[key][0].y)
-                    )
-                    c.lineTo(
-                        center.x + ((cellWidth / 2) * drawArr[key][1].x),
-                        center.y + ((cellWidth / 2) * drawArr[key][1].y)
-                    )
-                    c.stroke()
-                    
-                    c.beginPath()
-                    c.arc(
-                        center.x + ((cellWidth / 2) * drawArr[key][0].x),
-                        center.y + ((cellWidth / 2) * drawArr[key][0].y),
-                        c.lineWidth/2, 0, (2*Math.PI), false
-                    )
-                    c.fill()
-                }
-            })
-        })
-    })
-}
 // end maze generation
 
 // interactivity support
@@ -185,6 +149,23 @@ addEventListener('keypress', (event) => {
     }
 })
 
+addEventListener('mousemove', (event) => {
+    if (phase == 'start') {
+        if (event.clientX > 350 && event.clientX < 600 &&
+            event.clientY > 280 && event.clientY < 380) {
+                buttonFade = true
+        }
+        else {
+            buttonFade = false
+        }
+    }
+})
+
+addEventListener('mousedown', (event) => {
+    if (phase == 'start' && buttonFade) {
+        phase = 'generate'
+    }
+})
 
 class Player {
     constructor() {
@@ -203,8 +184,8 @@ class Player {
             element = this
         }
         return {
-            x: 300 + (cellWidth * element.c),
-            y: padding + (cellWidth * element.r)
+            x: padding.x + (cellWidth * element.c) + (cellWidth / 2),
+            y: padding.y + (cellWidth * element.r) + (cellWidth / 2)
         }
     }
 
@@ -240,7 +221,7 @@ class Player {
             else if (traceArr[i].t >= 0) {
                 c.beginPath()
                 c.arc(traceArr[i].x, traceArr[i].y, 7, 0, (2*Math.PI), false)
-                c.fillStyle = '#FFD369' // draw to trail
+                c.fillStyle = playerGradient // draw to trail
                 c.fill()
                 traceArr[i].t++
             }
@@ -251,7 +232,7 @@ class Player {
         if (this.moveLog.length == 0) {
             c.beginPath()
             c.arc(this.getPos().x, this.getPos().y, 7, 0, (2*Math.PI), false)
-            c.fillStyle = '#FFD369'
+            c.fillStyle = playerGradient
             c.fill()
         }
     }
@@ -271,42 +252,118 @@ class Player {
 
 c.fillStyle = '#393E46'
 c.fillRect(0, 0, canvas.width, canvas.height)
+const playerGradient = c.createLinearGradient(padding.x + 100,
+                                              padding.y + 100,
+                                              padding.x + 500,
+                                              padding.y + 500)
+playerGradient.addColorStop(0, '#FFD369')
+playerGradient.addColorStop(1, '#00D369')
 
-const gridDrawTime = 0
-grid.drawGrid()
-const img = document.getElementById('wasd')	
-c.drawImage(img, 900, 250, 264, 173)
+let gridDrawTime = 0
+// c.drawImage(wasdImg, 900, 250, 264, 173)
 
 function animate() { 
     requestAnimationFrame(animate)
     if (phase == 'start') {
+        // button color calculator
+        if (buttonFade) {
+            if (buttonGradient < 1000) {
+                buttonGradient += 20
+            }
+        }
+        else {
+            if (buttonGradient > 0) {
+                buttonGradient -= 20
+            }
+        }
+        const grad = c.createLinearGradient(0, 430 - buttonGradient, 0, 1430 - buttonGradient)
+        grad.addColorStop(0, '#FFD369')
+        grad.addColorStop(1, '#FFAE00')
         // draw start button with color based on hover
+        c.beginPath()
+        c.moveTo(400, 330)
+        c.lineTo(550, 330)
+        c.lineCap = 'round'
+        c.strokeStyle = grad
+        c.lineWidth = 100
+        c.stroke()
+        c.drawImage(generateImg, 375, 308, 204, 39)
+
     }
     else if (phase == 'generate') {
         if (gridDrawTime < 20) {
             // remove button with opacity
+            c.fillStyle = 'rgba(57, 62, 70, 0.2)'
+            c.fillRect(300, 230, 350, 200)
             gridDrawTime++
         }
         else if (gridDrawTime < 80) {
             if (gridDrawTime == 20) {
-                grid = initGrid()
+                c.strokeStyle = '#EEEEEE'
+                c.fillStyle = '#EEEEEE'
+                c.lineWidth = 8
             }
             // draw vertical lines flying down
+            const vertFramePos = padding.y + (((cellWidth * (mazeSize)) / 60) * (gridDrawTime - 19))
+            for (i=0; i<=mazeSize; i++) {
+                c.beginPath()
+                c.moveTo(padding.x + (cellWidth * i), padding.y)
+                c.lineTo(padding.x + (cellWidth * i), vertFramePos)
+                c.stroke() // why is this so slow
+            }
             gridDrawTime++
         }
         else if (gridDrawTime < 140) {
             // draw horizontal lines flying right
+            const horzFramePos = padding.x + (((cellWidth * (mazeSize)) / 60) * (gridDrawTime - 79))
+            for (i=0; i<=mazeSize; i++) {
+                c.beginPath()
+                c.moveTo(padding.x, padding.y + (cellWidth * i))
+                c.lineTo(horzFramePos, padding.y + (cellWidth * i))
+                c.stroke() // why is this so slow
+            }
             gridDrawTime++
         }
         else {
-            // take a generation step
-            // if generation done:
-            //     player = new Player,
-            //     draw spawn Animation,
-            //     phase = solve
+            if (gridDrawTime == 140) {
+                grid = initGrid()
+
+                ant = {x: 0, y: 0}
+                log = ['quit', {x: 0, y: 0}]
+
+                c.fillStyle = '#EEEEEE'
+                c.strokeStyle = '#393E46'
+                c.lineWidth = 9
+            }
+
+            if (log[log.length - 1] != 'quit') {
+                // cell loop here
+                dirKeys.shuffle()
+        
+                const trace = tryKeys()
+        
+                if (trace) {
+                    log.pop()
+                    ant = {
+                        x: log[log.length - 1].x,
+                        y: log[log.length - 1].y
+                    }
+                }
+                else {
+                    log.push({
+                        x: ant.x,
+                        y: ant.y
+                    })
+                }
+            }
+            else {
+                phase = 'solve'
+            }
+            gridDrawTime++
         }
     }
     else if (phase == 'solve') {
+        if (player == null) {player = new Player}
         player.update()
         
         // check if player is in bottom left,
