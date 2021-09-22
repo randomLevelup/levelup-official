a7d41615e57c5b8f392a94095a056fdf5241dd42
Jupiter-CB <jwestbard@gmail.com>
2021-09-22 18:48:53 -0400

    spline control point functionality

--- node-express/public/javascripts/spline.js
+++ node-express/public/javascripts/spline.js
@@ -5,14 +5,16 @@ canvas.width = innerWidth
 canvas.height = innerHeight
 
 class Spline {
-    constructor(x, y) {
+    constructor() {
         this.pairs = []
-        this.addPair(x, y)
     }
     /**movePoint function:
      * takes pair number, point selector, x, and y
      * updates the correct pair to the new x and y (maybe relative?)
      */
+    movePoint(pair, point, x, y) {
+        this.pairs[pair][point] = [x, y]
+    }
 
     /**addPair function:
      * takes x and y for endpoint pos
@@ -20,6 +22,10 @@ class Spline {
      * updates mode to ['movePoint' splines.length-1, this.pairs.length-1, 1]
      * IMPORTANT ^^
      */
+    addPair(x, y) {
+        this.pairs.push([[x, y], [x, y]])
+        mode = ['movePoint', splines.length-1, this.pairs.length-1, 1]
+    }
 
     /**draw function:
      *  draw handle lines
@@ -27,6 +33,25 @@ class Spline {
      *  draw curve lines
      *  draw curve endpoints
      */
+    draw() {
+        this.pairs.forEach(pair => {
+            c.fillStyle = 'white'
+            c.strokeStyle = 'white'
+
+            // draw lines (not implemented yet)
+
+            c.lineWidth = 2
+            c.beginPath()
+            c.arc(pair[1][0], pair[1][1], 4, 0, (2*Math.PI), false)
+            c.stroke()
+
+            // draw curve (not implemented yet)
+
+            c.beginPath()
+            c.arc(pair[0][0], pair[0][1], 4, 0, (2*Math.PI), false)
+            c.fill()
+        })
+    }
 }
 
 const splines = []
@@ -47,11 +72,11 @@ let rightclicked = false
  *      if no spline is active:
  *          create new spline using constructor
  */
-addEventListener("click", e => {
+addEventListener("mousedown", e => {
     let pointFound = false
-    splines.forEach(spline, i => {
-        spline.pairs.forEach(pair, j => {
-            pair.forEach(point, k => {
+    splines.forEach((spline, i) => {
+        spline.pairs.forEach((pair, j) => {
+            pair.forEach((point, k) => {
                 distance = Math.abs(Math.hypot((e.clientX - point[0]), (e.clientX - point[1])))
                 if (distance < 10) {
                     pointFound = true
@@ -62,12 +87,11 @@ addEventListener("click", e => {
     })
 
     if (!pointFound) {
-        if (activeSpline) {
-            splines[-1].addPair()
+        if (!activeSpline) {
+            splines.push(new Spline())
+            activeSpline = true
         }
-        else {
-            splines.push(new Spline(e.clientX, e.clientY))
-        }
+        splines[splines.length-1].addPair(e.clientX, e.clientY)
     }
 });
 
@@ -89,11 +113,21 @@ addEventListener("click", e => {
  *      exit the mode:
  *  
  */
+addEventListener('mouseup', e => {
+    if (mode[0] = 'movePoint') {
+        mode = ['orbit']
+    }
+})
 
 /**On mouseMove:
  *  if mode == movePoint:
  *      splines[mode[1]].movePoint(mode[2],mode[3], x, y)
  */
+addEventListener('mousemove', e => {
+    if (mode[0] == 'movePoint') {
+        splines[mode[1]].movePoint(mode[2], mode[3], e.clientX, e.clientY)
+    }
+})
 
 /**Animate Frames:
  *  theres a better way, but for now:
@@ -105,6 +139,10 @@ function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
+
+    splines.forEach(spline => {
+        spline.draw()
+    })
 }
 
 animate()
