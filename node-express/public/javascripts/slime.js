e5a61e937b058c807763fe26bd35adbee37364c6
Jupiter-CB <jwestbard@gmail.com>
2021-10-04 16:23:33 -0400

    slime project drawing system

--- node-express/public/javascripts/slime.js
+++ node-express/public/javascripts/slime.js
@@ -26,28 +26,29 @@ class Cell {
     draw(pos, size) {
         c.beginPath()
         c.fillStyle = `rgba(255, 255, 255, ${this.value / 16})`
-        c.fillRect(pos[0], pos[1], pos[0]+size, pos[1]+size)
+        console.log([pos, this.value / 16])
+        c.fillRect(pos[0], pos[1], size, size)
     }
 }
 
 class Grid {
     constructor(sizeX, sizeY) {
-        const vList = []
-        for (i=0; i<sizeY; i++) {
+        this.vList = []
+        for (let i=0; i<sizeY; i++) {
             const newRow = []
-            for (j=0; j<sizeX; j++) {
-                newRow.push(new Vertex(0)) // push the new vertices into rows
+            for (let j=0; j<sizeX; j++) {
+                newRow.push(new Vertex(Math.floor(Math.random() * 2)))
             }
-            vList.push(newRow) // push the new rows into grid
+            this.vList.push(newRow) // push the new rows into grid
         }
 
-        const cList = []
-        for (i=1; i<sizeY; i++) {
+        this.cList = []
+        for (let i=1; i<sizeY; i++) {
             const newRow = []
-            for (j=1; j<sizeX; j++) {
+            for (let j=1; j<sizeX; j++) {
                 newRow.push(new Cell(0))
             }
-            vList.push(newRow)
+            this.cList.push(newRow)
         }
         this.updateCells()
     }
@@ -57,14 +58,14 @@ class Grid {
     }
 
     updateCells() {
-        for (i=0; i<this.cList.length; i++) {
-            for (j=0; j<i.length; j++) {
-                const lutTotal = 0
+        for (let i=0; i<this.cList.length; i++) {
+            for (let j=0; j<this.cList[i].length; j++) {
+                let lutTotal = 0
                 this.getVertices(i, j).forEach((v, i) => {
                     // this method 'marches' around the square to get a LUT case int
                     lutTotal += (v.value * (2 ** i))
                 })
-                cList[i][j].update(lutTotal)
+                this.cList[i][j].update(lutTotal)
             }
         }
     }
@@ -80,18 +81,22 @@ class Grid {
     }
 
     draw() {
-        const drawPos = [200, 200]
-        for (i=0; i<this.cList.length; i++) {
-            for (j=0; j<i.length; j++) {
-                cList[i][j].draw(drawPos[0], drawPos[1])
-                drawPos[1] += 10
+        const drawPos = [200, 170]
+        for (let i=0; i<this.cList.length; i++) {
+            for (let j=0; j<this.cList[i].length; j++) {
+                this.cList[i][j].draw([drawPos[0], drawPos[1]], 40)
+                drawPos[1] += 40
             }
-            drawPos[0] += 10
+            drawPos[0] += 40
+            drawPos[1] = 170
         }
     }
 }
 
-const grid = new Grid(10, 10)
 c.fillStyle = 'black'
 c.fillRect(0, 0, canvas.width, canvas.height)
 
+const grid = new Grid(10, 10)
+// grid.vList[0][3] = new Vertex(1)
+grid.updateCells()
+grid.draw()
