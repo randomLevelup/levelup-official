92a0d33516d67c3bb7a432fe77e6c2c57d3c5ab1
Jupiter-CB <jwestbard@gmail.com>
2021-11-02 14:28:11 -0400

    spline project refactoring

--- node-express/public/javascripts/spline.js
+++ node-express/public/javascripts/spline.js
@@ -25,31 +25,16 @@ class Spline {
     constructor() {
         this.pairs = []
     }
-    /**movePoint function:
-     * takes pair number, point selector, x, and y
-     * updates the correct pair to the new x and y (maybe relative?)
-     */
+    
     movePoint(pair, point, x, y) {
         this.pairs[pair][point] = [x, y]
     }
 
-    /**addPair function:
-     * takes x and y for endpoint pos
-     * creates a new pair with these values
-     * updates mode to ['movePoint' splines.length-1, this.pairs.length-1, 1]
-     * IMPORTANT ^^
-     */
     addPair(x, y) {
         this.pairs.push([[x, y], [x, y]])
         mode = ['movePoint', splines.length-1, this.pairs.length-1, 1]
     }
 
-    /**draw function:
-     *  draw handle lines
-     *  draw handle endpoints
-     *  draw curve lines
-     *  draw curve endpoints
-     */
     draw() {
         this.pairs.forEach(pair => {
             c.strokeStyle = '#ebabc7'
@@ -84,7 +69,6 @@ class Spline {
                     let inArr = [
                         this.pairs[i-1][0],
                         this.pairs[i-1][1],
-                        // this.pairs[i][1], //[0] - (this.pairs[i][0] - this.pairs[i][1]),
                         [
                             this.pairs[i][0][0] - (this.pairs[i][1][0] - this.pairs[i][0][0]),
                             this.pairs[i][0][1] - (this.pairs[i][1][1] - this.pairs[i][0][1])
@@ -127,18 +111,6 @@ let mode = ['orbit'] // this is the default mode
 let activeSpline = false
 let rightclicked = false
 
-/**On leftClick:
- *  if mouse is over endpoint or handle:
- *      begin moveEndpoint / moveHandle mode
- * 
- *  if mouse is over nothing:
- *      if a spline is active:
- *          add pair at mousepos,
- *          begin movehandle mode
- * 
- *      if no spline is active:
- *          create new spline using constructor
- */
 addEventListener("mousedown", e => {
     if (!activeSpline) {
         let pointFound = false
@@ -163,50 +135,23 @@ addEventListener("mousedown", e => {
     }
 })
 
-
-/**On rightClick:
- *  if mouse is over handle:
- *      reset handle method (ACTUALLY DONT REALLY NEED THIS)
- * 
- *  if mouse is over endpoint:
- *      splice pair from spline
- * 
- *  if mouse is over nothing:
- *      if a spline is active:
- *          deactivate the spline
- */
 addEventListener('contextmenu', e => {
     e.preventDefault()
     activeSpline = false
 })
 
-/**On mouseUp:
- *  if mode = moveEndpoint / moveHandle:
- *      exit the mode:
- *  
- */
 addEventListener('mouseup', e => {
     if (mode[0] = 'movePoint') {
         mode = ['orbit']
     }
 })
 
-/**On mouseMove:
- *  if mode == movePoint:
- *      splines[mode[1]].movePoint(mode[2],mode[3], x, y)
- */
 addEventListener('mousemove', e => {
     if (mode[0] == 'movePoint') {
         splines[mode[1]].movePoint(mode[2], mode[3], e.clientX, e.clientY)
     }
 })
 
-/**Animate Frames:
- *  theres a better way, but for now:
- *  fill frame with #0F1123
- *  foreach splines:
- *      spline.draw()
- */
 function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = '#0F1123'
